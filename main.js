c461bebbe5abe7ecb9eb1a31b95365691a87c467
Hiskov <terence.jouguelet@gmail.fr>
2023-01-22 02:05:39 +0100

    Add imgzone full + wings

---
+++ main.js
@@ -0,0 +1,7 @@
+var audio = document.createElement("AUDIO")
+document.body.appendChild(audio);
+audio.src = "assets/touhou 2000s audio.mp3"
+
+/*document.body.addEventListener("mousemove", function () {
+    audio.play()
+})*/
