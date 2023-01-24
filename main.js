ef748e8e12bcf3a17112174895010be9e7f2d0c0
Hiskov <terence.jouguelet@gmail.fr>
2023-01-24 03:36:50 +0100

    Replace imgzone gif with video

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 var isSeeking = false;
 var seek = document.getElementById("position");
 var player = document.getElementById("player");
-
+var imgzone = document.getElementById("imgzone_vid");
 
 function setupSeek() {
-  seek.max = player.duration;
+  seek.max = Math.floor(player.duration);
 }
 
 function seekAudio() {
   isSeeking = true;
   player.currentTime = seek.value;
+  imgzone.currentTime = seek.value;
   isSeeking = false;
 }
 
@@ -26,12 +27,24 @@ var stop = document.querySelector(".audio-player .actions .stop");
 
 function clickPlay(){
     player.play();
+    imgzone.play();
+    player.volume = 0.1;
 }
 function clickPause(){
+  if(!player.paused){
     player.pause();
+    imgzone.pause();
+  }
+  else{
+    clickPlay();
+  }
 }
 function clickStop(){
     player.pause();
-    player.currentTime = 0
+    player.currentTime = 0;
+    imgzone.pause();
+    imgzone.currentTime = 0;
 }
+
+
 
