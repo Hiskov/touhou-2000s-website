ab617462470c4df6c4e25c1a9ef6b366baddb149
Hiskov <terence.jouguelet@gmail.fr>
2023-03-09 03:24:41 +0100

    Add thegap

--- router.js
+++ router.js
@@ -12,4 +12,8 @@ router.get("/help", function (req, res) {
     res.sendFile(path.join(__dirname, 'public/eirin.html'));
 });
 
+router.get("/thegap", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/thegap.html'));
+});
+
 module.exports = router;
